6f8bec06529eba5ea3db1d64809a5761e3ee7136
hay12396 <hay12396@gmail.com>
2017-03-23 16:35:51 +0200

    Changes: 1. ResourceManager.getAllResourceCategories() method added, will query for the categories registered with this character. 2. Added documentation to all of the IAliveAgent methods.

--- SMSReaderCharacter/app.js
+++ SMSReaderCharacter/app.js
@@ -4,6 +4,24 @@ var AliveClass = (function () {
         this.currentVoiceIndex = 0;
         this.changeInFirstTen = 0;
     }
+    /**
+     * This method gets called once when the character is being activated by the system.
+     * @param handler An object that allows the code to get reference to the managers.
+     * @param disabledPermissions A list of permissions that the user disabled.
+     */
+    AliveClass.prototype.onStart = function (handler, disabledPermissions) {
+        this.textToSpeechManager = handler.getTextToSpeechManager();
+        this.configurationManager = handler.getConfigurationManager();
+        this.menuManager = handler.getMenuManager();
+        for (var i = 0; i < disabledPermissions.length; i++)
+            if (disabledPermissions[i] == "SMS")
+                handler.getActionManager().terminate();
+    };
+    /**
+     * This method gets called every 250 milliseconds by the system, any logic updates to the state of your character should occur here.
+     * Note: onTick only gets called when the screen is ON.
+     * @param time The current time (in milliseconds) on the device.
+     */
     AliveClass.prototype.onTick = function (time) {
         if (this.voices == null || this.voices.length == 0)
             this.voices = this.textToSpeechManager.getVoices();
@@ -21,35 +39,59 @@ var AliveClass = (function () {
             this.changeInFirstTen++;
         }
     };
+    /**
+     * This method gets called by the system every 1 hour (may be in a different rate depending on the device).
+     * Note: this method only gets called when the screen is OFF.
+     * @param time The current time (in milliseconds) on the device.
+     */
+    AliveClass.prototype.onBackgroundTick = function (time) {
+    };
     AliveClass.prototype.getVoiceTextPresentation = function (v) {
         var gender = v.getName().indexOf("female") != -1 ? "female" : "male";
         return v.getISO3Language() + " " + gender + " " + this.currentVoiceIndex.toString() + "/" + (this.voices.length - 1).toString();
     };
-    AliveClass.prototype.onBackgroundTick = function (time) {
-    };
-    AliveClass.prototype.onStart = function (handler, disabledPermissions) {
-        this.textToSpeechManager = handler.getTextToSpeechManager();
-        this.configurationManager = handler.getConfigurationManager();
-        this.menuManager = handler.getMenuManager();
-        for (var i = 0; i < disabledPermissions.length; i++)
-            if (disabledPermissions[i] == "SMS")
-                handler.getActionManager().terminate();
-    };
+    /**
+     * This method gets called whenever a phone event (that you registered to) occur on the phone.
+     * @param eventName The name of the event that occurred.
+     * @param jsonedData The data of the event that occurred.
+     * For example, SMS_RECEIVED event will hold data about who sent the SMS, and the SMS content.
+     */
     AliveClass.prototype.onPhoneEventOccurred = function (eventName, jsonedData) {
         if (eventName == AgentConstants.SMS_RECEIVED) {
             this.textToSpeechManager.say(jsonedData);
         }
     };
+    /**
+     * This method gets called when the user is holding and moving the image of your character (on screen).
+     * @param oldX The X coordinate in the last tick (Top left).
+     * @param oldY The Y coordinate in the last tick (Top left).
+     * @param newX The X coordinate in the current tick (Top left).
+     * @param newY The Y coordinate in the current tick (Top left).
+     */
     AliveClass.prototype.onMove = function (oldX, oldY, newX, newY) {
     };
+    /**
+     * This method gets called when the user raised his finger off the character image (on screen).
+     * @param currentX The X coordinate of the character image on screen (Top left).
+     * @param currentY The Y coordinate of the character image on the screen (Top left).
+     */
     AliveClass.prototype.onRelease = function (currentX, currentY) {
     };
+    /**
+     * This method gets called whenever the user is holding the character image (on screen).
+     * @param currentX The current X coordinate of the character image (Top left).
+     * @param currentY The current Y coordinate of the character image (Top left).
+     */
     AliveClass.prototype.onPick = function (currentX, currentY) {
     };
-    AliveClass.prototype.onMenuItemSelected = function (itemName) {
+    /**
+     * This method gets called whenever the user has pressed a view in the character menu.
+     * @param viewName The 'Name' property of the view that was pressed.
+     */
+    AliveClass.prototype.onMenuItemSelected = function (viewName) {
         if (this.voices == null || this.voices.length == 0)
             this.voices = this.textToSpeechManager.getVoices();
-        switch (itemName) {
+        switch (viewName) {
             case "PrevButton":
                 if (this.currentVoiceIndex > 0)
                     this.currentVoiceIndex--;
@@ -67,6 +109,10 @@ var AliveClass = (function () {
         this.menuManager.setProperty("LangTextBox", "Text", name);
         this.textToSpeechManager.setVoice(this.currentVoiceIndex);
     };
+    /**
+     * This method gets called once just before the onStart method and is where the character menu views are defined.
+     * @param menuBuilder An object that fills the character menu.
+     */
     AliveClass.prototype.onConfigureMenuItems = function (menuBuilder) {
         var PrevButton = new ButtonMenuItem();
         PrevButton.Height = 1;
@@ -99,17 +145,46 @@ var AliveClass = (function () {
         menuBuilder.createButton(NextButton);
         menuBuilder.createTextBox(TextBox);
     };
+    /**
+     * This method gets called when the system done processing the speech recognition input.
+     * @param results A stringed version of what the user said.
+     */
     AliveClass.prototype.onSpeechRecognitionResults = function (results) { };
+    /**
+     * This method is called when the system received a reply from a previously HTTP request made by the character.
+     * @param response The reply body in a JSON form.
+     */
     AliveClass.prototype.onResponseReceived = function (response) {
     };
+    /**
+     * This method gets called when the system done collecting information about the device location.
+     * @param location The location information collected by the system.
+     */
     AliveClass.prototype.onLocationReceived = function (location) {
     };
+    /**
+     * This method gets called when the system done collecting information about the user activity.
+     * @param state Information about the user activity.
+     * Possible states: IN_VEHICLE, ON_BICYCLE, ON_FOOT, STILL, TILTING, WALKING, RUNNING, UNKNOWN.
+     */
     AliveClass.prototype.onUserActivityStateReceived = function (state) {
     };
+    /**
+     * This method gets called when the system done collecting information about nearby places around the device.
+     * @param places A list of places that are near the device.
+     */
     AliveClass.prototype.onPlacesReceived = function (places) {
     };
+    /**
+     * This method gets called when the system done collecting information about the headphone state.
+     * @param state 1 - the headphones are PLUGGED, 2 - the headphones are UNPLUGGED.
+     */
     AliveClass.prototype.onHeadphoneStateReceived = function (state) {
     };
+    /**
+     * This method gets called when the system done collecting information about the weather in the location of the device.
+     * @param weather Information about the weather.
+     */
     AliveClass.prototype.onWeatherReceived = function (weather) {
     };
     return AliveClass;
--- SMSReaderCharacter/app.ts
+++ SMSReaderCharacter/app.ts
@@ -15,6 +15,26 @@ class AliveClass implements IAliveAgent {
         this.changeInFirstTen = 0;
     }
 
+    /**
+     * This method gets called once when the character is being activated by the system.
+     * @param handler An object that allows the code to get reference to the managers.
+     * @param disabledPermissions A list of permissions that the user disabled.
+     */
+    onStart(handler: IManagersHandler, disabledPermissions: string[]): void {
+        this.textToSpeechManager = handler.getTextToSpeechManager();
+        this.configurationManager = handler.getConfigurationManager();
+        this.menuManager = handler.getMenuManager();
+
+        for (let i = 0; i < disabledPermissions.length; i++)
+            if (disabledPermissions[i] == "SMS")
+                handler.getActionManager().terminate();
+    }
+
+    /**
+     * This method gets called every 250 milliseconds by the system, any logic updates to the state of your character should occur here.
+     * Note: onTick only gets called when the screen is ON.
+     * @param time The current time (in milliseconds) on the device.
+     */
     onTick(time: number): void {
         if (this.voices == null || this.voices.length == 0)
             this.voices = this.textToSpeechManager.getVoices();
@@ -35,48 +55,70 @@ class AliveClass implements IAliveAgent {
         }
     }
 
+    /**
+     * This method gets called by the system every 1 hour (may be in a different rate depending on the device).
+     * Note: this method only gets called when the screen is OFF.
+     * @param time The current time (in milliseconds) on the device.
+     */
+    onBackgroundTick(time: number) {
+
+    }
+
     getVoiceTextPresentation(v: IVoice): string {
         let gender = v.getName().indexOf("female") != -1 ? "female" : "male";
         return v.getISO3Language() + " " + gender + " " + this.currentVoiceIndex.toString() + "/" + (this.voices.length - 1).toString();
     }
 
-    onBackgroundTick(time: number) {
-
-    }
-
-    onStart(handler: IManagersHandler, disabledPermissions: string[]): void {
-        this.textToSpeechManager = handler.getTextToSpeechManager();
-        this.configurationManager = handler.getConfigurationManager();
-        this.menuManager = handler.getMenuManager();
-
-        for (let i = 0; i < disabledPermissions.length; i++)
-            if (disabledPermissions[i] == "SMS")
-                handler.getActionManager().terminate();
-    }
-
+    /**
+     * This method gets called whenever a phone event (that you registered to) occur on the phone.
+     * @param eventName The name of the event that occurred.
+     * @param jsonedData The data of the event that occurred.
+     * For example, SMS_RECEIVED event will hold data about who sent the SMS, and the SMS content.
+     */
     onPhoneEventOccurred(eventName: string, jsonedData: string): void {
         if (eventName == AgentConstants.SMS_RECEIVED) {
             this.textToSpeechManager.say(jsonedData);
         }
     }
 
+    /**
+     * This method gets called when the user is holding and moving the image of your character (on screen).
+     * @param oldX The X coordinate in the last tick (Top left).
+     * @param oldY The Y coordinate in the last tick (Top left).
+     * @param newX The X coordinate in the current tick (Top left).
+     * @param newY The Y coordinate in the current tick (Top left).
+     */
     onMove(oldX: number, oldY: number, newX: number, newY: number): void {
 
     }
 
+    /**
+     * This method gets called when the user raised his finger off the character image (on screen).
+     * @param currentX The X coordinate of the character image on screen (Top left).
+     * @param currentY The Y coordinate of the character image on the screen (Top left).
+     */
     onRelease(currentX: number, currentY: number): void {
 
     }
 
+    /**
+     * This method gets called whenever the user is holding the character image (on screen).
+     * @param currentX The current X coordinate of the character image (Top left).
+     * @param currentY The current Y coordinate of the character image (Top left).
+     */
     onPick(currentX: number, currentY: number): void {
 
     }
 
-    onMenuItemSelected(itemName: string): void {
+    /**
+     * This method gets called whenever the user has pressed a view in the character menu.
+     * @param viewName The 'Name' property of the view that was pressed.
+     */
+    onMenuItemSelected(viewName: string): void {
         if (this.voices == null || this.voices.length == 0)
             this.voices = this.textToSpeechManager.getVoices();
 
-        switch (itemName) {
+        switch (viewName) {
             case "PrevButton":
                 if (this.currentVoiceIndex > 0)
                     this.currentVoiceIndex--;
@@ -97,6 +139,10 @@ class AliveClass implements IAliveAgent {
         this.textToSpeechManager.setVoice(this.currentVoiceIndex);
     }
 
+    /**
+     * This method gets called once just before the onStart method and is where the character menu views are defined.
+     * @param menuBuilder An object that fills the character menu.
+     */
     onConfigureMenuItems(menuBuilder: IMenuBuilder): void {
         let PrevButton = new ButtonMenuItem();
         PrevButton.Height = 1;
@@ -133,28 +179,57 @@ class AliveClass implements IAliveAgent {
         menuBuilder.createTextBox(TextBox);
     }
 
+    /**
+     * This method gets called when the system done processing the speech recognition input.
+     * @param results A stringed version of what the user said.
+     */
     onSpeechRecognitionResults(results: string): void { }
 
+    /**
+     * This method is called when the system received a reply from a previously HTTP request made by the character.
+     * @param response The reply body in a JSON form.
+     */
     onResponseReceived(response: string): void {
 
     }
 
+    /**
+     * This method gets called when the system done collecting information about the device location.
+     * @param location The location information collected by the system.
+     */
     onLocationReceived(location: IAliveLocation): void {
 
     }
 
+    /**
+     * This method gets called when the system done collecting information about the user activity.
+     * @param state Information about the user activity.
+     * Possible states: IN_VEHICLE, ON_BICYCLE, ON_FOOT, STILL, TILTING, WALKING, RUNNING, UNKNOWN.
+     */
     onUserActivityStateReceived(state: IAliveUserActivity) {
 
     }
 
+    /**
+     * This method gets called when the system done collecting information about nearby places around the device.
+     * @param places A list of places that are near the device.
+     */
     onPlacesReceived(places: IAlivePlaceLikelihood[]): void {
 
     }
 
+    /**
+     * This method gets called when the system done collecting information about the headphone state.
+     * @param state 1 - the headphones are PLUGGED, 2 - the headphones are UNPLUGGED.
+     */
     onHeadphoneStateReceived(state: number) {
 
     }
 
+    /**
+     * This method gets called when the system done collecting information about the weather in the location of the device.
+     * @param weather Information about the weather.
+     */
     onWeatherReceived(weather: IAliveWeather) {
 
     }
